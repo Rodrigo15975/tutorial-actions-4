7cbcb1d8ab42100bd1c46a04f0f6551bd958dc8c
rodrigo15975 <javierluis15975@hotmail.com>
2024-11-09 22:56:08 -0500

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState } from "react"
 import "./App.css"
 function App() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState()
   return (
     <>
       <h1>Testing in dessarollo</h1>
