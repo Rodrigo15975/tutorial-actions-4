bf99d5ffe1b318b5545eee673a3218b14f61584d
rodrigo15975 <javierluis15975@hotmail.com>
2024-11-09 23:02:56 -0500

    "Initialize count state to 0 and add increment button"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import "./App.css"
 function App() {
-  const [count, setCount] = useState()
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <h1>Testing in dessarollo</h1>
       <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </>
   )
 }
